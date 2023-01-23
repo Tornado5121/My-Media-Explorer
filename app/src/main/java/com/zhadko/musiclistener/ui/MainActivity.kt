df6d7474c2cba5d07f7be0b432e20f2f13e5f186
Tornado5121 <zhadkoroman@gmail.com>
2023-01-23 02:26:40 +0200

    git init

---
+++ app/src/main/java/com/zhadko/musiclistener/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zhadko.musiclistener.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zhadko.musiclistener.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
